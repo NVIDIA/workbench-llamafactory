6bb444b1f53351eba3c2418648d46fe5ceb655b9
Kedar Potdar <115327600+kedarpotdar-nv@users.noreply.github.com>
2024-05-19 12:15:04 -0700

    Update postBuild.bash

--- postBuild.bash
+++ postBuild.bash
@@ -7,6 +7,7 @@ set -e
 cd ~
 sudo apt install -y python3.10-venv
 python3 -m venv llama-factory-env
+cd /project
 git clone -b v0.7.0 https://github.com/hiyouga/LLaMA-Factory.git
 cd LLaMA-Factory
 $HOME/llama-factory-env/bin/pip install -e .[metrics]
