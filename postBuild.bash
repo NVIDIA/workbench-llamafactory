35964c3039d9176f4c821f78dcfca0c5fc1d2481
Kedar Potdar <kepotdar@nvidia.com>
2024-05-19 13:42:36 -0700

    committed

--- postBuild.bash
+++ postBuild.bash
@@ -7,7 +7,7 @@ set -e
 cd ~
 sudo apt install -y python3.10-venv
 python3 -m venv llama-factory-env
-git clone -b v0.7.0 https://github.com/hiyouga/LLaMA-Factory.git
+#git clone -b v0.7.0 https://github.com/hiyouga/LLaMA-Factory.git
 cd LLaMA-Factory
 $HOME/llama-factory-env/bin/pip install -e .[metrics]
 $HOME/llama-factory-env/bin/pip install bitsandbytes>=0.43.0
